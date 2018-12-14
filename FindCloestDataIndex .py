38fee3ff21d383f9fa75fac62a3720e7111b7eb4
CaptainPuggg <20291199+CaptainPuggg@users.noreply.github.com>
2018-12-14 04:58:43 +0000

    Update FindCloestDataIndex .py

--- FindCloestDataIndex .py
+++ FindCloestDataIndex .py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 myf=pd.read_csv('Big.csv')
-
 myf['date'] = pd.to_datetime(myf['date'])
-
 mydate=pd.Index(myf['date'])
 
+# define the date you want
 date_=pd.to_datetime('2009-01-05')
+index_=mydate.get_loc(date_,method='nearest')
 
-mydate.get_loc(date_,method='nearest')
+
