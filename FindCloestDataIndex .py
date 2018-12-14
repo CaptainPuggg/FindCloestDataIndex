48df673663cf544719eba18dec533607c65eb98f
GloriaPP <20291199+CaptainPuggg@users.noreply.github.com>
2018-12-14 15:08:43 +0800

    Update FindCloestDataIndex .py

--- FindCloestDataIndex .py
+++ FindCloestDataIndex .py
@@ -14,3 +14,11 @@ for y in range(2009,2012):
         date_=pd.to_datetime(str(y)+'-'+str(m)+'-'+'05')
         index_=mydate.get_loc(date_,method='nearest')
         print(str(index_)+':'+str(myf['date'][index_]))
+# for meidiudiu:
+
+for y in range(2016,2019):
+    for m in range(1,13):
+        date_=pd.to_datetime(str(y)+'-'+str(m)+'-'+'05')
+        if date_>(pd.to_datetime('2016-02-29')) and date_<(pd.to_datetime('2018-07')):
+                index_=mydate.get_loc(date_,method='nearest')
+                print(str(index_)+':'+str(myf['date'][index_]))
